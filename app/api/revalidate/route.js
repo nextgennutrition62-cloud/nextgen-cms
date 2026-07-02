e42a59e461f6f1c7c02e52f8e0a1ae1e3eafc366
nextgennutrition62-cloud <nextgennutrition62@gmail.com>
2026-07-02 11:54:07 +0200

    Create route.js

---
+++ app/api/revalidate/route.js
@@ -0,0 +1,8 @@
+import { revalidatePath } from 'next/cache';
+import { NextResponse } from 'next/server';
+
+export async function POST() {
+  revalidatePath('/');
+  revalidatePath('/blog');
+  return NextResponse.json({ revalidated: true });
+}
